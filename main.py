04eed25002e90976efa4d405216c9ea819ad29f3
A Chethan Reddy <69640722+chethanreddy123@users.noreply.github.com>
2022-07-04 12:32:00 +0530

    stops Words Changed

--- main.py
+++ main.py
@@ -5,10 +5,6 @@ from fastapi.middleware.cors import CORSMiddleware
 import joblib
 
 
-import nltk
-nltk.download('stopwords')
-nltk.download('punkt')
-
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
@@ -57,7 +53,7 @@ async def checkStatus(info : Request):
     req_info = await info.json()
     CurrString = dict(req_info)["comment"]
 
-    stop_words = set(stopwords.words('english'))
+    stop_words = {"ourselves", "hers", "between", "yourself", "but", "again", "there", "about", "once", "during", "out", "very", "having", "with", "they", "own", "an", "be", "some", "for", "do", "its", "yours", "such", "into", "of", "most", "itself", "other", "off", "is", "s", "am", "or", "who", "as", "from", "him", "each", "the", "themselves", "until", "below", "are", "we", "these", "your", "his", "through", "don", "nor", "me", "were", "her", "more", "himself", "this", "down", "should", "our", "their", "while", "above", "both", "up", "to", "ours", "had", "she", "all", "no", "when", "at", "any", "before", "them", "same", "and", "been", "have", "in", "will", "on", "does", "yourselves", "then", "that", "because", "what", "over", "why", "so", "can", "did", "not", "now", "under", "he", "you", "herself", "has", "just", "where", "too", "only", "myself", "which", "those", "i", "after", "few", "whom", "t", "being", "if", "theirs", "my", "against", "a", "by", "doing", "it", "how", "further", "was", "here", "than"} 
     word_tokens = word_tokenize(CurrString)
     filtered_sentence = [w for w in word_tokens if not w.lower() in stop_words]
 
